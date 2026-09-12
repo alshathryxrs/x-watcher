850124a8beabdaad5e69e467b73eb572ccc8770c
alshathryxrs <150628604+alshathryxrs@users.noreply.github.com>
2026-09-12 13:43:01 +0300

    Update live_watcher4.js

--- live_watcher4.js
+++ live_watcher4.js
@@ -25,7 +25,7 @@ async function sendBarkSafe(title, message) {
       body: JSON.stringify({
         title:   title,
         body:    message,
-        sound:   'fanfare',
+        sound:   'gotosleep',
         level:   'critical',
       })
     });
